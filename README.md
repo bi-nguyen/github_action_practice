cef0575e40cea3e0dc7b698129a4c7cf3ba2ba29
bi-nguyen <itbi2705@gmail.com>
2024-10-02 10:42:21 +0700

    adding files for github action demo

---
+++ README.md
@@ -0,0 +1,1 @@
+# This repo demo github action process
---
+++ test_function.py
@@ -0,0 +1,17 @@
+from function1 import addfunc, subfunc, mulfunc
+
+
+def test_add():
+    assert addfunc(1, 2) == 3
+    assert addfunc( 2, 3) == 5
+    assert addfunc(3, 4) == 7
+
+def test_sub():
+    assert subfunc(1, 2) == -1
+    assert subfunc( 2, 3) == -1
+    assert subfunc(3, 4) == -1
+
+def test_mul():
+    assert mulfunc(1, 2) == 2
+    assert mulfunc( 2, 3) == 6
+    assert mulfunc(3, 4) == 12
